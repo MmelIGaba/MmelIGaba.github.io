12d929f491ae2ab0ccd7ce5f6ebe9477aae599d8
Mmeli <125876492+MmelIGaba@users.noreply.github.com>
2024-04-05 01:43:53 +0200

    Update act.js

--- act.js
+++ act.js
@@ -2,28 +2,22 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('active');
 });
 
-$(document).ready(function() {
-  $('#contact-form').submit(function(event) {
-    event.preventDefault(); 
-    const formData = $(this).serialize();
-
-    $.ajax({
-      type: 'POST',
-      url: $(this).attr('action'),
-      data: formData,
-      dataType: 'json',
-      encode: true
-    })
-   .done(function(data) {
-      if (data.success) {
-        alert('Message sent successfully!');
-        $('#contact-form')[0].reset();
-      } else {
-        alert('Error: ' + data.message);
-      }
-    })
-   .fail(function(xhr, textStatus, errorThrown) {
-      alert('Error: ' + textStatus + ' ' + errorThrown);
-    });
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  
+  const formData = new FormData(event.target);
+  
+  fetch(event.target.action, {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => {
+    console.log('Form submitted successfully');
+  })
+  .catch(error => {
+    console.error('Error submitting the form:', error);
   });
 });
