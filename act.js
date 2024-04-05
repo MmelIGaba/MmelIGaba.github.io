6ec9902b955309939585cebf5b7568d8d13b205d
Mmela Dyantyi <1762984@students.wits.ac.za>
2024-04-05 23:03:47 +0200

    Form fixing and nav

--- act.js
+++ act.js
@@ -1,23 +1,27 @@
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    document.querySelector('.nav').classList.toggle('active');
-});
-
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-  event.preventDefault(); 
-  
-  const formData = new FormData(event.target);
-  
-  fetch(event.target.action, {
-    method: 'POST',
-    body: formData,
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  .then(response => {
-    console.log('Form submitted successfully');
-  })
-  .catch(error => {
-    console.error('Error submitting the form:', error);
+document
+  .querySelector(".hamburger-menu")
+  .addEventListener("click", function () {
+    document.querySelector(".nav").classList.toggle("active");
   });
-});
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    fetch(event.target.action, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        console.log("Form submitted successfully");
+        document.getElementById("contactForm").reset();
+      })
+      .catch((error) => {
+        console.error("Error submitting the form:", error);
+      });
+  });
